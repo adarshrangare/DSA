90dbd7d02f3de9abd757d57a7daff9fc5cc136a6
Adarsh Rangare <akrangare@gmail.com>
2023-08-20 20:12:47 +0530

    Create MergeStringAlternatively.java

---
+++ LeetCodeQuestions/MergeStringAlternatively.java
@@ -0,0 +1,30 @@
+class MergeStringAlternatively {
+    public String mergeAlternately(String word1, String word2) {
+
+        int i =0;
+        int j =0;
+        StringBuilder sb = new StringBuilder();
+        while(i<word1.length() && j<word2.length()){
+
+            sb.append(word1.charAt(i));
+            i++;
+            sb.append(word2.charAt(j));
+            j++;
+
+        }
+
+        if(i<word1.length()){
+            
+            sb.append(word1.substring(i));
+
+        }
+        if(j<word2.length()){
+            
+            sb.append(word2.substring(j));
+
+        }
+
+        return sb.toString();
+
+    }
+}
